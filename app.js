29fe86f7b91c6c3695fb0cc8d6c4908416e38eae
Prasad N <prasad.n@tatacommunications.com>
2023-03-22 19:28:52 +0530

    view change

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var password = require('./routes/password');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 app.use(cors({origin:"*"}));
 
 app.use(logger('dev'));
